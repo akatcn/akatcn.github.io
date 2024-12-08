40e67670c192277708d6b58ef9c70c31495bcd45
YongHoon <akatcn@gmail.com>
2024-12-09 00:15:37 +0900

    🛠️  페이지 구조 설정 및 더미 마크다운 파일 생성

---
+++ src/app/article/[group]/[series]/[article]/page.tsx
@@ -0,0 +1,13 @@
+import MyComponent from '@/component/MyComponent'
+import React from 'react'
+
+function ArticleDetailPage() {
+  return (
+    <div>
+      <p>ArticleDetailPage</p>
+      <MyComponent str="some prop" />
+    </div>
+  )
+}
+
+export default ArticleDetailPage
